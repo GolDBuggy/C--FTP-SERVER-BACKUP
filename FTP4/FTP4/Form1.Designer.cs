fddea8aa9db56cb8fbf856cb99787d5adf0388ca
GolDBuggy <87481140+GolDBuggy@users.noreply.github.com>
2021-08-20 09:56:34 +0300

    Send server backup to FTP with C#
    Send server backup to FTP with C#

---
+++ FTP4/FTP4/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace FTP4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(117, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Aktar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(227, 284);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Eski Yedek Silindi!";
+            this.label1.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 22);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 48);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Dosyları çek";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 76);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(303, 84);
+            this.listBox1.TabIndex = 3;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(12, 173);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(303, 84);
+            this.listBox2.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 263);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(99, 58);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Silinecek Yedekler";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(215, 22);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 48);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Sql Devre Dışı Bırak";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(117, 263);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(92, 58);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Klasörü Sil";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 356);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ FTP4/FTP4/Form1.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+
+
+
+namespace FTP4
+{
+    public partial class Form1 : Form
+    {
+        public object downloadFiles { get; private set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+
+        //MM/dd
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime bugun = DateTime.Now;
+             bugun.ToString();
+             DateTime today = bugun.Date;
+             
+             FtpWebRequest request = (FtpWebRequest)FtpWebRequest.Create("ftp://serverIp"+"/" +today.ToString("MM/dd/yyyy") );
+             request.Credentials = new NetworkCredential("username", "password");
+             request.UsePassive = true;
+             request.UseBinary = true;
+             request.KeepAlive = false;
+             Console.WriteLine("Getting the response");
+
+             request.Method = WebRequestMethods.Ftp.MakeDirectory;
+             //var resp = (FtpWebResponse)request.GetResponse();
+
+             using (var resp = (FtpWebResponse)request.GetResponse())
+             {
+                 Console.WriteLine(resp.StatusCode);
+             }
+            Thread.Sleep(1000);
+            string[] veriler1 = new string[listBox1.Items.Count];
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                veriler1[i] = listBox1.Items[i].ToString();
+            }
+
+            try
+            {
+                for (int i = 1; i <=listBox1.Items.Count; i++)
+                {
+                    string select = listBox1.GetItemText(listBox1.SelectedItem);
+
+                    FileInfo FI = new FileInfo(@"C:/Users/İbrahim Hakkı/Desktop/avc/" + veriler1[i]);
+
+
+
+                   
+                    string uri = "ftp://serverIp/" + today.ToString("MM/dd/yyyy") + "/" + FI.Name;
+                    
+                    FtpWebRequest FTP;
+                    
+                    FTP = (FtpWebRequest)FtpWebRequest.Create(new Uri(uri));
+                   
+                    FTP.Credentials = new NetworkCredential("username", "password");
+                   
+                    FTP.KeepAlive = false;
+                   
+                    FTP.Method = WebRequestMethods.Ftp.UploadFile;
+                  
+                    FTP.UseBinary = true;
+                    
+                    FTP.ContentLength = FI.Length;
+                   
+                    int buffLength = 2048;
+                    byte[] buff = new byte[buffLength];
+                    int contentLen;
+                    
+                    FileStream FS = FI.OpenRead();
+                    try
+                    {
+                        Stream strm = FTP.GetRequestStream();
+                        contentLen = FS.Read(buff, 0, buffLength);
+                        while (contentLen != 0)//dosya bitene kadar gönderme işlemi
+                        {
+                            strm.Write(buff, 0, contentLen);
+                            contentLen = FS.Read(buff, 0, buffLength);
+                        }
+                        strm.Close();
+                        FS.Close();
+                        
+                    }
+                    catch (Exception ex)
+                    {
+                       MessageBox.Show(ex.Message, "Hata");
+                    }
+
+                }
+           }
+            catch
+            {
+
+            }
+
+        }
+
+
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            listBox1.SelectionMode = SelectionMode.MultiExtended;
+            
+        }
+        private void sil()
+        {
+            DateTime bugun = DateTime.Now.AddDays(-7);
+            bugun.ToString();
+            DateTime today = bugun.Date;
+
+
+            List<string> files = new List<string>();
+            try
+            {
+                FtpWebRequest request = FtpWebRequest.Create("ftp://serverIp/" + today.ToString("MM/dd/yyyy")) as FtpWebRequest;
+                request.Method = WebRequestMethods.Ftp.ListDirectory;
+                request.Credentials = new NetworkCredential("username", "password");
+                FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+                Stream responseStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(responseStream);
+                while (!reader.EndOfStream)
+                {
+                    Application.DoEvents();
+                    files.Add(reader.ReadLine());
+                }
+                response.Close();
+                responseStream.Close();
+                reader.Close();
+                foreach (string file in files)
+                {
+                    listBox2.Items.Add(file);
+
+                }
+                listBox1.Items.Clear();
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+
+       
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo di = new DirectoryInfo("C:/Users/");
+            FileInfo[] files = di.GetFiles();
+            foreach (FileInfo fi in files)
+            {
+                listBox1.Items.Add(fi.Name);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FtpWebRequest FTP;
+            DateTime bugun = DateTime.Now.AddDays(-7);
+            bugun.ToString();
+            DateTime today = bugun.Date;
+
+            sil();
+
+
+            string[] veriler2 = new string[listBox2.Items.Count];
+            for (int i = 0; i < listBox2.Items.Count; i++)
+            {
+                veriler2[i] = listBox2.Items[i].ToString();
+            }
+            try
+            {
+                for (int i = 0; i <= listBox2.Items.Count; i++)
+                {
+
+                    FTP = (FtpWebRequest)FtpWebRequest.Create(new Uri("ftp://backup.iletmen.com.tr/" + today.ToString("MM/dd/yyyy") + "/" + veriler2[i]));
+
+
+                    FTP.UseBinary = true;
+
+                    FTP.Credentials = new NetworkCredential("backup", "Xyz9d8!1");
+
+                    FTP.Method = WebRequestMethods.Ftp.DeleteFile;
+
+                    FtpWebResponse response = (FtpWebResponse)FTP.GetResponse();
+
+                    Console.WriteLine(response.StatusDescription);
+
+                }
+                
+
+
+
+
+
+            }
+            catch (Exception)
+            {
+
+
+            }
+
+
+
+        }
+
+        
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            new Process() { StartInfo = new ProcessStartInfo("D:\\file-address") { Verb = "runas" } }.Start();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            FtpWebRequest FTP;
+            DateTime bugun = DateTime.Now.AddDays(-7);
+            bugun.ToString();
+            DateTime today = bugun.Date;
+            listBox2.Items.Clear();
+            if (listBox2.Items.Count == 0)
+            {
+                try
+
+                {
+                    
+
+                    FTP = (FtpWebRequest)FtpWebRequest.Create(new Uri("ftp://serverIp" + today.ToString("MM/dd/yyy")));
+
+                    FTP.UseBinary = true; FTP.Credentials = new NetworkCredential("username", "password");
+
+                    FTP.Method = WebRequestMethods.Ftp.RemoveDirectory;
+
+                    FtpWebResponse response = (FtpWebResponse)FTP.GetResponse();
+
+                    Console.WriteLine(response.StatusDescription);
+                    label1.Visible = true;
+
+                }
+
+                catch (Exception ex)
+
+                {
+
+
+
+                }
+            }
+        }
+    }
+         
+
+}
+
